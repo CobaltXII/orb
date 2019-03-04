8b212a49e5932da6fdbce2ec899a9985234bbe12
CobaltXII <sidatt64@gmail.com>
2019-03-03 19:57:53 -0500

    Now using predefined scenes

--- orb.cpp
+++ orb.cpp
@@ -444,14 +444,7 @@ int main(int argc, char** argv)
 {
 	srand(2048);
 
-	orbs.push_back({0.0f, -3024.0f, 0.0f, 0.0f, 0.0f, 0.0f, 3000.0f});
-
-	orbs.push_back({0.0f, -8.0f, -56.0f, 0.0f, 1.0f, 0.0f, 16.0f});
-
-	orbs.push_back({0.0f - 32.0f, -8.0f, -56.0f, 1.0f, 0.0f, 0.0f, 16.0f});
-	orbs.push_back({0.0f + 32.0f, -8.0f, -56.0f, 0.0f, 0.0f, 1.0f, 16.0f});
-
-	lamps.push_back({25.0f, 50.0f, 0.0f, 1e+4f * 1.4f, 1e+4f * 1.4f, 1e+4f * 1.4f, 50.0f});
+	#include "scene/three.cpp"
 
 	int x_res = 128 * 8;
 	int y_res = 128 * 8;
