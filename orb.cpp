7ba504d71c3f75889aca3657edea81ea40bda151
CobaltXII <sidatt64@gmail.com>
2019-03-03 19:50:49 -0500

    Added reflections and recursive architecture

--- orb.cpp
+++ orb.cpp
@@ -49,51 +49,412 @@ void nuke(std::string note)
 	exit(EXIT_FAILURE);
 }
 
+std::vector<orb> orbs;
+
+std::vector<lamp> lamps;
+
+float cast
+(
+	float ray_ox,
+	float ray_oy,
+	float ray_oz,
+
+	float ray_dx,
+	float ray_dy,
+	float ray_dz,
+
+	float& hit_orb_x,
+	float& hit_orb_y,
+	float& hit_orb_z,
+
+	float& hit_orb_r,
+	float& hit_orb_g,
+	float& hit_orb_b,
+
+	float& hit_orb_radius
+)
+{
+	float min_dist = std::numeric_limits<float>::max();
+
+	for (int i = 0; i < orbs.size(); i++)
+	{
+		orb orb = orbs[i];
+
+		float i_lx = orb.x - ray_ox;
+		float i_ly = orb.y - ray_oy;
+		float i_lz = orb.z - ray_oz;
+
+		float i_adj2 =
+		(
+			i_lx * ray_dx +
+			i_ly * ray_dy +
+			i_lz * ray_dz
+		);
+
+		float i_d2 =
+		(
+			i_lx * i_lx +
+			i_ly * i_ly +
+			i_lz * i_lz
+		);
+
+		i_d2 -= i_adj2 * i_adj2;
+
+		float i_r2 =
+		(
+			orb.radius *
+			orb.radius
+		);
+
+		if (i_d2 > i_r2)
+		{
+			continue;
+		}
+
+		float i_thc = sqrtf(i_r2 - i_d2);
+
+		float i_t0 = i_adj2 - i_thc;
+		float i_t1 = i_adj2 + i_thc;
+
+		if (i_t0 < 0.0f && i_t1 < 0.0f)
+		{
+			continue;
+		}
+
+		float i_d = std::min(i_t0, i_t1);
+
+		if (i_d < min_dist)
+		{
+			min_dist = i_d;
+
+			hit_orb_x = orb.x;
+			hit_orb_y = orb.y;
+			hit_orb_z = orb.z;
+
+			hit_orb_r = orb.r;
+			hit_orb_g = orb.g;
+			hit_orb_b = orb.b;
+
+			hit_orb_radius = orb.radius;
+		}
+	}
+
+	return min_dist;
+}
+
+void trace
+(
+	float ray_ox,
+	float ray_oy,
+	float ray_oz,
+
+	float ray_dx,
+	float ray_dy,
+	float ray_dz,
+
+	float& out_r,
+	float& out_g,
+	float& out_b,
+
+	int depth = 0
+)
+{
+	// Cast primary ray.
+
+	float hit_orb_x;
+	float hit_orb_y;
+	float hit_orb_z;
+
+	float hit_orb_r;
+	float hit_orb_g;
+	float hit_orb_b;
+
+	float hit_orb_radius;
+
+	float min_dist = cast
+	(
+		ray_ox,
+		ray_oy,
+		ray_oz,
+
+		ray_dx,
+		ray_dy,
+		ray_dz,
+
+		hit_orb_x,
+		hit_orb_y,
+		hit_orb_z,
+
+		hit_orb_r,
+		hit_orb_g,
+		hit_orb_b,
+
+		hit_orb_radius
+	);
+
+	out_r = 0.0f;
+	out_g = 0.0f;
+	out_b = 0.0f;
+
+	if (min_dist == std::numeric_limits<float>::max())
+	{
+		// Missed everything.
+
+		return;
+	}
+
+	// Hit something. Find the color using contributions from light, shadow,
+	// reflection rays, etc.
+
+	float hit_x = ray_ox + ray_dx * min_dist;
+	float hit_y = ray_oy + ray_dy * min_dist;
+	float hit_z = ray_oz + ray_dz * min_dist;
+
+	float norm_x = hit_x - hit_orb_x;
+	float norm_y = hit_y - hit_orb_y;
+	float norm_z = hit_z - hit_orb_z;
+
+	float norm_length = sqrtf
+	(
+		norm_x * norm_x +
+		norm_y * norm_y +
+		norm_z * norm_z
+	);
+
+	norm_x /= norm_length;
+	norm_y /= norm_length;
+	norm_z /= norm_length;
+
+	// Check for large spheres, which should be checkered.
+
+	if (hit_orb_radius > 200.0f)
+	{
+		float domain_x = 10.0f;
+		float domain_y = 10.0f;
+		float domain_z = 10.0f;
+
+		float expand = 512.0f;
+
+		int check_x = fmod(hit_x + domain_x * expand, domain_x * 2.0f) >= domain_x;
+		int check_y = fmod(hit_y + domain_y * expand, domain_y * 2.0f) >= domain_y;
+		int check_z = fmod(hit_z + domain_z * expand, domain_z * 2.0f) >= domain_z;
+
+		if (check_x ^ check_y ^ check_z)
+		{
+			hit_orb_r = 0.0f * 2.0f;
+			hit_orb_g = 0.0f * 2.0f;
+			hit_orb_b = 0.0f * 2.0f;
+		}
+		else
+		{
+			hit_orb_r = 0.85f * 2.0f;
+			hit_orb_g = 0.85f * 2.0f;
+			hit_orb_b = 0.85f * 2.0f;
+		}
+	}
+
+	// Calculate lighting.
+
+	float shadow_rays = 16.0f;
+
+	for (int k = 0; k < int(shadow_rays); k++)
+	{
+		for (int i = 0; i < lamps.size(); i++)
+		{
+			lamp lamp = lamps[i];
+
+			// Perturb lamp for smooth shadows.
+
+			float r1 = rand11();
+			float r2 = rand11();
+			float r3 = rand11();
+
+			float deviation = shadow_rays - 1.0f;
+
+			lamp.x += r1 * deviation;
+			lamp.y += r2 * deviation;
+			lamp.z += r3 * deviation;
+
+			// Direction to lamp.
+
+			float dtl_x = lamp.x - hit_x;
+			float dtl_y = lamp.y - hit_y;
+			float dtl_z = lamp.z - hit_z;
+
+			lamp.x -= r1 * deviation;
+			lamp.y -= r2 * deviation;
+			lamp.z -= r3 * deviation;
+
+			float dr = sqrtf
+			(
+				dtl_x * dtl_x +
+				dtl_y * dtl_y +
+				dtl_z * dtl_z
+			);
+
+			dtl_x /= dr;
+			dtl_y /= dr;
+			dtl_z /= dr;
+
+			// Send shadow rays.
+
+			float shadow = 1.0f;
+
+			for (int j = 0; j < orbs.size(); j++)
+			{
+				orb orb = orbs[j];
+
+				// Prevent shadow acne.
+
+				float eps = 1e-3f;
+
+				// Send shadow ray.
+
+				float shadow_ray_dummy;
+
+				float shadow_dist = cast
+				(
+					hit_x + eps * dtl_x,
+					hit_y + eps * dtl_y,
+					hit_z + eps * dtl_z,
+
+					dtl_x,
+					dtl_y,
+					dtl_z,
+
+					shadow_ray_dummy,
+					shadow_ray_dummy,
+					shadow_ray_dummy,
+
+					shadow_ray_dummy,
+					shadow_ray_dummy,
+					shadow_ray_dummy,
+
+					shadow_ray_dummy
+				);
+
+				if (shadow_dist < dr)
+				{
+					shadow -= 0.2f;
+				}
+			}
+
+			float l_pow =
+			(
+				norm_x * dtl_x +
+				norm_y * dtl_y +
+				norm_z * dtl_z
+			);
+
+			l_pow /= dr * dr;
+
+			out_r += fmax(0.0f, hit_orb_r * lamp.r * l_pow * shadow);
+			out_g += fmax(0.0f, hit_orb_g * lamp.g * l_pow * shadow);
+			out_b += fmax(0.0f, hit_orb_b * lamp.b * l_pow * shadow);
+
+			l_pow = powf(l_pow * dr * dr, 128.0f) / dr / dr * lamp.radius / shadow_rays;
+
+			out_r += fmax(0.0f, lamp.r * l_pow * shadow);
+			out_g += fmax(0.0f, lamp.g * l_pow * shadow);
+			out_b += fmax(0.0f, lamp.b * l_pow * shadow);
+		}
+	}
+
+	out_r /= shadow_rays;
+	out_g /= shadow_rays;
+	out_b /= shadow_rays;
+
+	// Add reflections.
+
+	if (depth > 5)
+	{
+		return;
+	}
+
+	float eps = 1e-3f;
+
+	float reflect_ox = hit_x + norm_x * eps;
+	float reflect_oy = hit_y + norm_y * eps;
+	float reflect_oz = hit_z + norm_z * eps;
+
+	float incident_dot_norm =
+	(
+		ray_dx * norm_x +
+		ray_dy * norm_y +
+		ray_dz * norm_z
+	);
+
+	float reflect_dx = ray_dx - (2.0f * incident_dot_norm * norm_x);
+	float reflect_dy = ray_dy - (2.0f * incident_dot_norm * norm_y);
+	float reflect_dz = ray_dz - (2.0f * incident_dot_norm * norm_z);
+
+	float reflect_r = 0.0f;
+	float reflect_g = 0.0f;
+	float reflect_b = 0.0f;
+
+	float reflection_rays = 1.0f;
+
+	if (depth != 0)
+	{
+		reflection_rays = 1.0f;
+	}
+
+	for (int i = 0; i < reflection_rays; i++)
+	{
+		float gloss = 0.0f;
+
+		float additive_reflect_r;
+		float additive_reflect_g;
+		float additive_reflect_b;
+
+		trace
+		(
+			reflect_ox,
+			reflect_oy,
+			reflect_oz,
+
+			reflect_dx + rand11() * gloss,
+			reflect_dy + rand11() * gloss,
+			reflect_dz + rand11() * gloss,
+
+			additive_reflect_r,
+			additive_reflect_g,
+			additive_reflect_b,
+
+			depth + 1
+		);
+
+		reflect_r += additive_reflect_r;
+		reflect_g += additive_reflect_g;
+		reflect_b += additive_reflect_b;
+	}
+
+	reflect_r /= reflection_rays;
+	reflect_g /= reflection_rays;
+	reflect_b /= reflection_rays;
+
+	float reflectivity = 0.8f;
+
+	out_r = out_r * (1.0f - reflectivity) + reflect_r * reflectivity;
+	out_g = out_g * (1.0f - reflectivity) + reflect_g * reflectivity;
+	out_b = out_b * (1.0f - reflectivity) + reflect_b * reflectivity;
+}
+
 int main(int argc, char** argv)
 {
 	srand(2048);
 
-	std::vector<orb> orbs;
+	orbs.push_back({0.0f, -3024.0f, 0.0f, 0.0f, 0.0f, 0.0f, 3000.0f});
 
-	std::vector<lamp> lamps;
+	orbs.push_back({0.0f, -8.0f, -56.0f, 0.0f, 1.0f, 0.0f, 16.0f});
 
-	orbs.push_back({0.0f, 0.0f, -3064.0f, 0.5f, 0.75f, 0.25f, 3000.0f});
+	orbs.push_back({0.0f - 32.0f, -8.0f, -56.0f, 1.0f, 0.0f, 0.0f, 16.0f});
+	orbs.push_back({0.0f + 32.0f, -8.0f, -56.0f, 0.0f, 0.0f, 1.0f, 16.0f});
 
-	for (int i = 0; i < 128; i++)
-	{
-		float r1 = rand01();
-		float r2 = rand01();
-		float r3 = rand01();
+	lamps.push_back({25.0f, 50.0f, 0.0f, 1e+4f * 1.4f, 1e+4f * 1.4f, 1e+4f * 1.4f, 50.0f});
 
-		float radius = 32.0f;
-
-		if (true)
-		{
-			r2 = sqrtf(r2);
-		}
-
-		orbs.push_back
-		(
-			{
-				sinf(r1 * 2.0f * M_PI) * r2 * radius,
-				cosf(r1 * 2.0f * M_PI) * r2 * radius,
-
-				-24.0f + cosf(r3 * 2.0f * M_PI) * r2 * radius,
-
-				(sinf(r1 * 2.0f * M_PI) * r2 * radius + radius) / radius,
-				(cosf(r1 * 2.0f * M_PI) * r2 * radius + radius) / radius,
-
-				(-32.0f + cosf(r3 * 2.0f * M_PI) * r2 * radius + radius) / radius,
-
-				3.0f
-			}
-		);
-	}
-
-	lamps.push_back({0.0f, 0.0f, 104.0f, 1e+4f * 1.4f, 1e+4f * 1.4f, 1e+4f * 1.4f, 50.0f});
-
-	int x_res = 128 * 32;
-	int y_res = 128 * 32;
+	int x_res = 128 * 8;
+	int y_res = 128 * 8;
 
 	float x_resf = x_res;
 	float y_resf = y_res;
@@ -138,291 +499,45 @@ int main(int argc, char** argv)
 			float ray_oy = 0.0f;
 			float ray_oz = 0.0f;
 
-			float ray_x = ((float(i + 0.5f) / float(x_res)) * 2.0f - 1.0f) * fov_adjust * aspect;
+			float ray_dx = ((float(i + 0.5f) / float(x_res)) * 2.0f - 1.0f) * fov_adjust * aspect;
 
-			float ray_y = (1.0f - (float(j + 0.5f) / float(y_res)) * 2.0f) * fov_adjust;
+			float ray_dy = (1.0f - (float(j + 0.5f) / float(y_res)) * 2.0f) * fov_adjust;
 
-			float ray_z = -1.0f;
+			float ray_dz = -1.0f;
 
 			float ray_length = sqrtf
 			(
-				ray_x * ray_x +
-				ray_y * ray_y +
-				ray_z * ray_z
+				ray_dx * ray_dx +
+				ray_dy * ray_dy +
+				ray_dz * ray_dz
 			);
 
-			ray_x /= ray_length;
-			ray_y /= ray_length;
-			ray_z /= ray_length;
-
-			// Find nearest geometry intersection.
-
-			float min_dist = std::numeric_limits<float>::max();
-
-			float min_ox;
-			float min_oy;
-			float min_oz;
-
-			float min_or;
-			float min_og;
-			float min_ob;
-
-			float min_radius;
-
-			for (int i = 0; i < orbs.size(); i++)
-			{
-				orb orb = orbs[i];
-
-				float i_lx = orb.x - ray_ox;
-				float i_ly = orb.y - ray_oy;
-				float i_lz = orb.z - ray_oz;
-
-				float i_adj2 =
-				(
-					i_lx * ray_x +
-					i_ly * ray_y +
-					i_lz * ray_z
-				);
-
-				float i_d2 =
-				(
-					i_lx * i_lx +
-					i_ly * i_ly +
-					i_lz * i_lz
-				);
-
-				i_d2 -= i_adj2 * i_adj2;
-
-				float i_r2 =
-				(
-					orb.radius *
-					orb.radius
-				);
-
-				if (i_d2 > i_r2)
-				{
-					continue;
-				}
-
-				float i_thc = sqrtf(i_r2 - i_d2);
-
-				float i_t0 = i_adj2 - i_thc;
-				float i_t1 = i_adj2 + i_thc;
-
-				if (i_t0 < 0.0f && i_t1 < 0.0f)
-				{
-					continue;
-				}
-
-				float i_d = std::min(i_t0, i_t1);
-
-				if (i_d < min_dist)
-				{
-					min_dist = i_d;
-
-					min_ox = orb.x;
-					min_oy = orb.y;
-					min_oz = orb.z;
-
-					min_or = orb.r;
-					min_og = orb.g;
-					min_ob = orb.b;
-
-					min_radius = orb.radius;
-				}
-			}
-
-			// Generate color.
-
-			if (min_dist != std::numeric_limits<float>::max())
-			{
-				float r = 0.0f;
-				float g = 0.0f;
-				float b = 0.0f;
-
-				float hit_x = ray_ox + ray_x * min_dist;
-				float hit_y = ray_oy + ray_y * min_dist;
-				float hit_z = ray_oz + ray_z * min_dist;
-
-				if (min_radius > 200.0f)
-				{
-					float domain_x = 10.0f;
-					float domain_y = 10.0f;
-					float domain_z = 10.0f;
-
-					float expand = 512.0f;
-
-					int check_x = fmod(hit_x + domain_x * expand, domain_x * 2.0f) >= domain_x;
-					int check_y = fmod(hit_y + domain_y * expand, domain_y * 2.0f) >= domain_y;
-					int check_z = fmod(hit_z + domain_z * expand, domain_z * 2.0f) >= domain_z;
-
-					if (check_x ^ check_y ^ check_z)
-					{
-						min_or = 0.15f * 2.0f;
-						min_og = 0.15f * 2.0f;
-						min_ob = 0.15f * 2.0f;
-					}
-					else
-					{
-						min_or = 0.85f * 2.0f;
-						min_og = 0.85f * 2.0f;
-						min_ob = 0.85f * 2.0f;
-					}
-				}
-
-				float norm_x = hit_x - min_ox;
-				float norm_y = hit_y - min_oy;
-				float norm_z = hit_z - min_oz;
-
-				float norm_length = sqrtf
-				(
-					norm_x * norm_x +
-					norm_y * norm_y +
-					norm_z * norm_z
-				);
-
-				norm_x /= norm_length;
-				norm_y /= norm_length;
-				norm_z /= norm_length;
-
-				// Calculate lighting.
-
-				float shadow_rays = 16.0f;
-
-				for (int k = 0; k < int(shadow_rays); k++)
-				{
-					for (int i = 0; i < lamps.size(); i++)
-					{
-						lamp lamp = lamps[i];
-
-						// Perturb lamp for smooth shadows.
-
-						float r1 = rand11();
-						float r2 = rand11();
-						float r3 = rand11();
-
-						float deviation = shadow_rays - 1.0f;
-
-						lamp.x += r1 * deviation;
-						lamp.y += r2 * deviation;
-						lamp.z += r3 * deviation;
-
-						float dtl_x = lamp.x - hit_x;
-						float dtl_y = lamp.y - hit_y;
-						float dtl_z = lamp.z - hit_z;
-
-						lamp.x -= r1 * deviation;
-						lamp.y -= r2 * deviation;
-						lamp.z -= r3 * deviation;
-
-						float dr = sqrtf
-						(
-							dtl_x * dtl_x +
-							dtl_y * dtl_y +
-							dtl_z * dtl_z
-						);
-
-						dtl_x /= dr;
-						dtl_y /= dr;
-						dtl_z /= dr;
-
-						// Send shadow rays.
-
-						bool shadow = false;
-
-						for (int j = 0; j < orbs.size(); j++)
-						{
-							orb orb = orbs[j];
-
-							// Prevent shadow acne.
-
-							float eps = 1e-3f;
-
-							float i_lx = orb.x - hit_x - dtl_x * eps;
-							float i_ly = orb.y - hit_y - dtl_y * eps;
-							float i_lz = orb.z - hit_z - dtl_z * eps;
-
-							float i_adj2 =
-							(
-								i_lx * dtl_x +
-								i_ly * dtl_y +
-								i_lz * dtl_z
-							);
-
-							float i_d2 =
-							(
-								i_lx * i_lx +
-								i_ly * i_ly +
-								i_lz * i_lz
-							);
-
-							i_d2 -= i_adj2 * i_adj2;
-
-							float i_r2 =
-							(
-								orb.radius *
-								orb.radius
-							);
-
-							if (i_d2 > i_r2)
-							{
-								continue;
-							}
-
-							float i_thc = sqrtf(i_r2 - i_d2);
-
-							float i_t0 = i_adj2 - i_thc;
-							float i_t1 = i_adj2 + i_thc;
-
-							if (i_t0 < 0.0f && i_t1 < 0.0f)
-							{
-								continue;
-							}
-
-							float i_d = fmin(i_t0, i_t1);
-
-							if (i_d < dr)
-							{
-								shadow = true;
-							}
-						}
-
-						if (shadow)
-						{
-							continue;
-						}
-
-						float l_pow =
-						(
-							norm_x * dtl_x +
-							norm_y * dtl_y +
-							norm_z * dtl_z
-						);
-
-						l_pow /= dr * dr;
-
-						r += fmax(0.0f, min_or * lamp.r * l_pow);
-						g += fmax(0.0f, min_og * lamp.g * l_pow);
-						b += fmax(0.0f, min_ob * lamp.b * l_pow);
-
-						// Fake specularity.
-
-						l_pow = powf(l_pow * dr * dr, 256.0f) / dr / dr * shadow_rays;
-
-						r += fmax(0.0f, min_or * lamp.r * l_pow);
-						g += fmax(0.0f, min_og * lamp.g * l_pow);
-						b += fmax(0.0f, min_ob * lamp.b * l_pow);
-					}
-				}
-
-				r /= shadow_rays;
-				g /= shadow_rays;
-				b /= shadow_rays;
-
-				pixel[R] = fmax(0.0f, fmin(255.0f, r * 255.0f));
-				pixel[G] = fmax(0.0f, fmin(255.0f, g * 255.0f));
-				pixel[B] = fmax(0.0f, fmin(255.0f, b * 255.0f));
-			}
+			ray_dx /= ray_length;
+			ray_dy /= ray_length;
+			ray_dz /= ray_length;
+
+			float color_r;
+			float color_g;
+			float color_b;
+
+			trace
+			(
+				ray_ox,
+				ray_oy,
+				ray_oz,
+
+				ray_dx,
+				ray_dy,
+				ray_dz,
+
+				color_r,
+				color_g,
+				color_b
+			);
+
+			pixel[R] = fmax(0.0f, fmin(255.0f, color_r * 255.0f));
+			pixel[G] = fmax(0.0f, fmin(255.0f, color_g * 255.0f));
+			pixel[B] = fmax(0.0f, fmin(255.0f, color_b * 255.0f));
 		}
 	}
 
