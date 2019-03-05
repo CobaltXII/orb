7f82e379508a8d7a6aede8ca27bfc5a2b9558a2c
CobaltXII <sidatt64@gmail.com>
2019-03-04 19:02:08 -0500

    Fixed a lot of things

--- scene/ambience.cpp
+++
@@ -1,8 +0,0 @@
-lamps.push_back({0.0f, 0.0f, 0.0f + 10004.0f, 1e+8f * 2.4f, 1e+8f * 2.4f, 1e+8f * 2.4f, 10000.0f});
-lamps.push_back({0.0f, 0.0f, 0.0f - 10004.0f, 1e+8f * 2.4f, 1e+8f * 2.4f, 1e+8f * 2.4f, 10000.0f});
-
-lamps.push_back({0.0f, 0.0f + 10004.0f, 0.0f, 1e+8f * 2.4f, 1e+8f * 2.4f, 1e+8f * 2.4f, 10000.0f});
-lamps.push_back({0.0f, 0.0f - 10004.0f, 0.0f, 1e+8f * 2.4f, 1e+8f * 2.4f, 1e+8f * 2.4f, 10000.0f});
-
-lamps.push_back({0.0f + 10004.0f, 0.0f, 0.0f, 1e+8f * 2.4f, 1e+8f * 2.4f, 1e+8f * 2.4f, 10000.0f});
-lamps.push_back({0.0f - 10004.0f, 0.0f, 0.0f, 1e+8f * 2.4f, 1e+8f * 2.4f, 1e+8f * 2.4f, 10000.0f});
