3ed1ff7641df10c1d9b64c586f44d50f2243c178
CobaltXII <sidatt64@gmail.com>
2019-03-08 18:37:52 -0500

    Added capped cones

--- orb.cpp
+++ orb.cpp
@@ -8,6 +8,8 @@
 #include <limits>
 #include <random>
 
+#define sign(x) ((x > 0.0f) - (x < 0.0f))
+
 #define fmin(a, b) ((a) < (b) ? (a) : (b))
 #define fmax(a, b) ((a) > (b) ? (a) : (b))
 
@@ -215,6 +217,11 @@ struct cone: shape
 		this->material4 = material4;
 	}
 };
+
+inline cone TO_CONE(shape* __victim)
+{
+	return *((cone*)__victim);
+}
 
 struct plane: shape
 {
@@ -733,6 +740,212 @@ float cast
 				set_ptr(norm_x, ((ray_ox + ray_dx * i_d) - ellipsoid1.x) / ellipsoid1.radius_x);
 				set_ptr(norm_y, ((ray_oy + ray_dy * i_d) - ellipsoid1.y) / ellipsoid1.radius_y);
 				set_ptr(norm_z, ((ray_oz + ray_dz * i_d) - ellipsoid1.z) / ellipsoid1.radius_z);
+			}
+		}
+		else if (shape1->primitive == shape_type::st_cone)
+		{
+			cone cone1 = TO_CONE(shape1);
+
+			float ba_x = cone1.b_x - cone1.a_x;
+			float ba_y = cone1.b_y - cone1.a_y;
+			float ba_z = cone1.b_z - cone1.a_z;
+
+			float oa_x = ray_ox - cone1.a_x;
+			float oa_y = ray_oy - cone1.a_y;
+			float oa_z = ray_oz - cone1.a_z;
+
+			float ob_x = ray_ox - cone1.b_x;
+			float ob_y = ray_oy - cone1.b_y;
+			float ob_z = ray_oz - cone1.b_z;
+
+			float baba =
+			(
+				ba_x * ba_x +
+				ba_y * ba_y +
+				ba_z * ba_z
+			);
+
+			float rdba =
+			(
+				ray_dx * ba_x +
+				ray_dy * ba_y +
+				ray_dz * ba_z
+			);
+
+			float oaba =
+			(
+				oa_x * ba_x +
+				oa_y * ba_y +
+				oa_z * ba_z
+			);
+
+			float obba =
+			(
+				ob_x * ba_x +
+				ob_y * ba_y +
+				ob_z * ba_z
+			);
+    
+			if (oaba < 0.0f)
+			{
+				float c1_x = oa_x * rdba - ray_dx * oaba;
+				float c1_y = oa_y * rdba - ray_dy * oaba;
+				float c1_z = oa_z * rdba - ray_dz * oaba;
+
+				float c1 =
+				(
+					c1_x * c1_x +
+					c1_y * c1_y +
+					c1_z * c1_z
+				);
+
+				if (c1 < cone1.radius_a * cone1.radius_a * rdba * rdba)
+				{
+					float t = -oaba / rdba;
+
+					if (t < min_dist)
+					{
+						min_dist = t;
+
+						set_ptr(hit_shape_material1, cone1.material1);
+						set_ptr(hit_shape_material2, cone1.material2);
+						set_ptr(hit_shape_material3, cone1.material3);
+						set_ptr(hit_shape_material4, cone1.material4);
+
+						set_ptr(hit_shape_r, cone1.r);
+						set_ptr(hit_shape_g, cone1.g);
+						set_ptr(hit_shape_b, cone1.b);
+
+						set_ptr(hit_shape, shape1);
+
+						// Surface normal.
+
+						float isqr_baba = 1.0f / sqrtf(baba);
+
+						set_ptr(norm_x, -ba_x * isqr_baba);
+						set_ptr(norm_y, -ba_y * isqr_baba);
+						set_ptr(norm_z, -ba_z * isqr_baba);
+					}
+				}
+			}
+			else if (obba > 0.0f)
+			{
+				float t = -obba / rdba;
+
+				float c1_x = ob_x + ray_dx * t;
+				float c1_y = ob_y + ray_dy * t;
+				float c1_z = ob_z + ray_dz * t;
+
+				float c1 =
+				(
+					c1_x * c1_x +
+					c1_y * c1_y +
+					c1_z * c1_z
+				);
+
+				if (c1 < cone1.radius_b * cone1.radius_b)
+				{
+					if (t < min_dist)
+					{
+						min_dist = t;
+
+						set_ptr(hit_shape_material1, cone1.material1);
+						set_ptr(hit_shape_material2, cone1.material2);
+						set_ptr(hit_shape_material3, cone1.material3);
+						set_ptr(hit_shape_material4, cone1.material4);
+
+						set_ptr(hit_shape_r, cone1.r);
+						set_ptr(hit_shape_g, cone1.g);
+						set_ptr(hit_shape_b, cone1.b);
+
+						set_ptr(hit_shape, shape1);
+
+						// Surface normal.
+
+						float isqr_baba = 1.0f / sqrtf(baba);
+
+						set_ptr(norm_x, ba_x * isqr_baba);
+						set_ptr(norm_y, ba_y * isqr_baba);
+						set_ptr(norm_z, ba_z * isqr_baba);
+					}
+				}
+			}
+
+			float rbra = cone1.radius_b - cone1.radius_a;
+
+			float oc_x = oa_x * cone1.radius_b - ob_x * cone1.radius_a;
+			float oc_y = oa_y * cone1.radius_b - ob_y * cone1.radius_a;
+			float oc_z = oa_z * cone1.radius_b - ob_z * cone1.radius_a;
+
+			float hyhy = baba + rbra * rbra;
+
+			float ocba =
+			(
+				oc_x * ba_x +
+				oc_y * ba_y +
+				oc_z * ba_z
+			);
+
+			float ocrd =
+			(
+				oc_x * ray_dx +
+				oc_y * ray_dy +
+				oc_z * ray_dz
+			);
+
+			float ococ =
+			(
+				oc_x * oc_x +
+				oc_y * oc_y +
+				oc_z * oc_z
+			);
+
+			float k2 = baba * baba - hyhy * rdba * rdba;
+
+			float k1 = baba * baba * ocrd - hyhy * rdba * ocba;
+			float k0 = baba * baba * ococ - hyhy * ocba * ocba;
+
+			float h = k1 * k1 - k2 * k0;
+
+			if (h < 0.0f)
+			{
+				continue;
+			}
+
+			float t = (-k1 - sign(rbra) * sqrtf(h)) / (k2 * rbra);
+
+			if (t < 0.0f)
+			{
+				continue;
+			}
+
+			float y = oaba + t * rdba;
+
+			if (y > 0.0f && y < baba)
+			{
+				if (t < min_dist)
+				{
+					min_dist = t;
+
+					set_ptr(hit_shape_material1, cone1.material1);
+					set_ptr(hit_shape_material2, cone1.material2);
+					set_ptr(hit_shape_material3, cone1.material3);
+					set_ptr(hit_shape_material4, cone1.material4);
+
+					set_ptr(hit_shape_r, cone1.r);
+					set_ptr(hit_shape_g, cone1.g);
+					set_ptr(hit_shape_b, cone1.b);
+
+					set_ptr(hit_shape, shape1);
+
+					// Surface normal.
+
+					float isqr_baba = 1.0f / sqrtf(baba);
+
+					set_ptr(norm_x, baba * (baba * (oa_x + t * ray_dx) - rbra * ba_x * cone1.radius_a) - ba_x * hyhy * y);
+					set_ptr(norm_y, baba * (baba * (oa_y + t * ray_dy) - rbra * ba_y * cone1.radius_a) - ba_y * hyhy * y);
+					set_ptr(norm_z, baba * (baba * (oa_z + t * ray_dz) - rbra * ba_z * cone1.radius_a) - ba_z * hyhy * y);
+				}
 			}
 		}
 	}
