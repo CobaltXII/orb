8ac6b27bce5f839ea28ea1d166abe38d56b09bd2
CobaltXII <sidatt64@gmail.com>
2019-03-09 13:09:09 -0500

    Update orb.sh

--- orb.sh
+++ orb.sh
@@ -1,1 +1,1 @@
-clang++ orb.cpp lib/stb_image.o lib/stb_image_write.o lib/stb_perlin.o -o orb -std=c++11 -Ofast -march=native && ./orb
+clang++ orb.cpp inih/ini.o lib/stb_image.o lib/stb_image_write.o lib/stb_perlin.o -o orb -std=c++11 -Ofast -march=native && ./orb ini/spheres.ini
