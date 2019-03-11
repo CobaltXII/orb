9303e7993c81dc4f89343f48bd3ae22586a27277
CobaltXII <sidatt64@gmail.com>
2019-03-11 19:46:41 -0400

    Create shape.hpp

---
+++ hpp/shape.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+enum shape_type
+{
+	st_sphere, st_plane, st_ellipsoid, st_cone, st_capsule, st_cylinder, st_triangle
+};
+
+struct shape
+{
+	float material1;
+	float material2;
+	float material3;
+	float material4;
+	float material5;
+
+	float r;
+	float g;
+	float b;
+
+	shape_type primitive;
+};
